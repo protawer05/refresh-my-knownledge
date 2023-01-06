23453e355e32c53f3e73d352d87e73f4d96639f8
Anton <sychiov2015@yandex.ru>
2023-01-07 03:25:43 +0500

    some edit

--- src/components/pages/product/Product.tsx
+++ src/components/pages/product/Product.tsx
@@ -1,15 +1,21 @@
-import { FC } from 'react'
-import { useParams } from 'react-router-dom'
-import Layout from '../../ui/layout/Layout'
+import { FC, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { ProductService } from '../../../services/product.service'
-import Button from '../../ui/button/Button'
-import Gallery from './gallery/Gallery'
 import { useTypedSelector } from '../../../hooks/useTypedSelector'
 import { useActions } from '../../../hooks/useActions'
 
+import Layout from '../../ui/layout/Layout'
+import Button from '../../ui/button/Button'
+import Gallery from './gallery/Gallery'
+import backArrow from '../../../assets/Back_Arrow.svg'
+
+import styles from './Product.module.scss'
+
 const Product: FC = () => {
 	const { id: productId } = useParams()
+	const navigate = useNavigate()
+	const [onButton, setOnButton] = useState(false)
 	const {
 		data: product,
 		isError,
@@ -37,7 +43,15 @@ const Product: FC = () => {
 
 	return (
 		<Layout>
-			<h1 className={'text-3xl font-semibold mb-3 mt-4'}>{product.title}</h1>
+			<div className='flex items-center'>
+				<div
+					className='mt-1 mr-3 cursor-pointer'
+					onClick={() => navigate('../')}
+				>
+					<img className={styles.backArrow} src={backArrow} alt={'back'} />
+				</div>
+				<h1 className={'text-3xl font-semibold mb-3 mt-4'}>{product.title}</h1>
+			</div>
 			<Gallery images={product?.images} />
 			<div className={'text-xl text-center mt-3'}>
 				{new Intl.NumberFormat('en-US', {
@@ -50,8 +64,14 @@ const Product: FC = () => {
 				onClick={() =>
 					isInCart ? removeFromCart(Number(productId)) : addToCart(product)
 				}
+				onMouseEnter={() => setOnButton(true)}
+				onMouseLeave={() => setOnButton(false)}
 			>
-				{isInCart ? 'This product is already in cart' : 'Add to cart'}
+				{isInCart
+					? !onButton
+						? 'This product is already in cart'
+						: 'Delete this from card?'
+					: 'Add to cart'}
 			</Button>
 		</Layout>
 	)
